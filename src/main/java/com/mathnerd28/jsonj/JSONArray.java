4e0d5b13c8c90f971f7f783e7a0ddf73c379d03b
MathNerd28 <xander.bhalla@gmail.com>
2023-06-28 12:27:49 -0400

    Add convenience setters to JSONArray

--- src/main/java/com/mathnerd28/jsonj/JSONArray.java
+++ src/main/java/com/mathnerd28/jsonj/JSONArray.java
@@ -13,6 +13,79 @@ public class JSONArray extends ArrayList<JSONBase> implements JSONBase {
     super(c);
   }
 
+  public JSONObject getObject(int index) {
+    return (JSONObject) get(index);
+  }
+
+  public JSONArray getArray(int index) {
+    return (JSONArray) get(index);
+  }
+
+  public String getString(int index) {
+    return ((JSONString) get(index)).get();
+  }
+
+  public boolean getBoolean(int index) {
+    return ((JSONBoolean) get(index)).getAsBoolean();
+  }
+
+  public long getLong(int index) {
+    return ((JSONInteger) get(index)).getAsLong();
+  }
+
+  public int getInt(int index) {
+    return ((JSONInteger) get(index)).getAsInt();
+  }
+
+  public double getDouble(int index) {
+    JSONBase val = get(index);
+    if (val instanceof JSONFloat) {
+      return ((JSONFloat) val).getAsDouble();
+    } else {
+      return ((JSONInteger) get(index)).getAsLong();
+    }
+  }
+
+  public void addString(String str) {
+    add(new JSONString(str));
+  }
+
+  public void addBoolean(boolean b) {
+    add(JSONBoolean.valueOf(b));
+  }
+
+  public void addLong(long l) {
+    add(new JSONInteger(l));
+  }
+
+  public void addInt(int i) {
+    add(new JSONInteger(i));
+  }
+
+  public void addDouble(double d) {
+    add(new JSONFloat(d));
+  }
+
+  public void addString(int index, String str) {
+    add(index, new JSONString(str));
+  }
+
+  public void addBoolean(int index, boolean b) {
+    add(index, JSONBoolean.valueOf(b));
+  }
+
+  public void addLong(int index, long l) {
+    add(index, new JSONInteger(l));
+  }
+
+  public void addInt(int index, int i) {
+    add(index, new JSONInteger(i));
+  }
+
+  public void addDouble(int index, double d) {
+    add(index, new JSONFloat(d));
+  }
+
   @Override
   public String toJSON() {
     return toJSON(false);
