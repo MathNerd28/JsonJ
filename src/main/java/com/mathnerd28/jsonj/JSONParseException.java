3b33ad1ce32cc9dbcc4c737b1506b36d0f4799c7
MathNerd28 <xander.bhalla@gmail.com>
2023-06-29 14:08:36 -0400

    Remove unnecessary storage of line and col

--- src/main/java/com/mathnerd28/jsonj/JSONParseException.java
+++ src/main/java/com/mathnerd28/jsonj/JSONParseException.java
@@ -4,12 +4,7 @@ public class JSONParseException extends Exception {
 
   private static final long serialVersionUID = -5710080026111384881L;
 
-  private final int line;
-  private final int col;
-
   public JSONParseException(String msg, int line, int col) {
     super(msg + " at line " + line + ", column " + col);
-    this.line = line;
-    this.col = col;
   }
 }
